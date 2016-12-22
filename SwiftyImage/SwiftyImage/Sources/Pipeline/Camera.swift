309f5a69be46d8dd72df2e91d6a3a760447ce88d
tqtifnypmb <tqtifnypmb@gmail.com>
2016-12-22 23:37:48 +0800

    delete unused files

--- SwiftyImage/SwiftyImage/Sources/Pipeline/Camera.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Camera.swift
-//  SwiftyImage
-//
-//  Created by tqtifnypmb on 22/12/2016.
-//  Copyright © 2016 tqitfnypmb. All rights reserved.
-//
-
-import Foundation
--- SwiftyImage/SwiftyImage/Sources/Pipeline/VideoCamera.swift
+++
@@ -1,9 +0,0 @@
-//
-//  VideoCamera.swift
-//  SwiftyImage
-//
-//  Created by tqtifnypmb on 22/12/2016.
-//  Copyright © 2016 tqitfnypmb. All rights reserved.
-//
-
-import Foundation
