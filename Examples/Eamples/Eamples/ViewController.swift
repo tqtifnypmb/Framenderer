c27a9687c7a758eac20e42e7ce472e2001e4402b
tqtifnypmb <tqtifnypmb@gmail.com>
2017-02-13 21:45:28 +0800

    Files structure

---
+++ Examples/Eamples/Eamples/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  SwiftyImage
+//
+//  Created by tqtifnypmb on 08/12/2016.
+//  Copyright © 2016 tqitfnypmb. All rights reserved.
+//
+
+import UIKit
+import CoreImage
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var originImageView: UIImageView!
+    @IBOutlet weak var processedImageView: UIImageView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let origin = UIImage(named: "lena")
+        let blend = UIImage(named: "aero1")
+        
+        let context = CIContext()
+        
+        let filter = CIFilter(name: "CIHueAdjust")!
+        
+        filter.setValue(CIImage(cgImage: origin!.cgImage!), forKey: kCIInputImageKey)
+        filter.setValue(2, forKey: kCIInputAngleKey)
+       //filter.setValue(CIImage(cgImage: origin!.cgImage!), forKey: kCIInputBackgroundImageKey)
+        let result = filter.outputImage!
+        let cgImage = context.createCGImage(result, from: result.extent)
+        originImageView.image = UIImage(cgImage: cgImage!)
+        
+        do {
+            let canva = ImageCanvas(image: origin!)
+
+            let test1 = GaussianBlurFilter()
+            let test = SaturationBlendFilter(otherImage: blend!.cgImage!)
+            let motion = MotionBlurFilter(angle: 0, distance: 25)
+            canva.filters = [test1,test1,test1,test1]
+            try canva.process()
+            
+            let result = canva.processedImage()
+            let processed = UIImage(cgImage: result)
+            processedImageView.image = processed
+        } catch {
+            print(glGetError())
+            print(error.localizedDescription)
+        }
+    }
+}
+
---
+++ SwiftyImage/SwiftyImage.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyImage.h
+//  SwiftyImage
+//
+//  Created by tqtifnypmb on 13/02/2017.
+//  Copyright © 2017 tqtifnypmb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyImage.
+FOUNDATION_EXPORT double SwiftyImageVersionNumber;
+
+//! Project version string for SwiftyImage.
+FOUNDATION_EXPORT const unsigned char SwiftyImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyImage/PublicHeader.h>
+
+
